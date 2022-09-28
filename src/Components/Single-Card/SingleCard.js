0ddfadb7447573475c41046b6495e038c5a41343
Shapna <shapna088@gmail.com>
2022-09-29 03:25:01 +0600

    card destructuring

--- src/Components/Single-Card/SingleCard.js
+++ src/Components/Single-Card/SingleCard.js
@@ -2,13 +2,13 @@ import React from 'react';
 import './SingleCard.css'
 
 const SingleCard = ({card}) => {
-    console.log(card)
+    const {image, name, time} = card;
     return (
         <div className='single-card'>
-            <img className='card-image' src={card.image} alt="" />
+            <img className='card-image' src={image} alt="" />
             <div className="card-info">
-                <h5>Name : {card.name} </h5>
-                <p>Time: {card.time} min </p>
+                <h5>Name : {name} </h5>
+                <p>Time: {time} min </p>
                 <button className='added-btn'>Add To list</button>
             </div>
         </div>
