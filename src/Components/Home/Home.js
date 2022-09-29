b43bc6ebedca64a6b9381270ced689eefee65ee2
Shapna <shapna088@gmail.com>
2022-09-29 17:00:49 +0600

    some style added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -63,21 +63,26 @@ const Home = () => {
                 </div>
             </div>
             <div className="ques-ans">
-                <div>
-                    <strong>1. How does react work</strong>
-                    <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p> <br />
-                    <strong>2. Difference between props and state?</strong>
-                    <p>Props: Props are read only, It can be modified, Props Immutable <br />
-                    State: State changes can be asynchronous. It can be modified using this.setState. State Mutuable.
-                    </p>
-                    <strong>3. What is the uses of UseEffect</strong>
-                    <p>The useEffect Hook always perform as SideEffect. For example: <br />
-                        Add an event listener for a button. <br />
-                        Data fetching from API when component mounts. <br />
-                        Perform an action when state or props change. <br />
-                        Clean up event listeners when the component unmounts.
-
-                    </p>
+                <div className='ques-container'>
+                    <div>
+                        <strong>1. How does react work</strong>
+                        <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
+                    </div>
+                    <div>
+                        <strong>2. Difference between props and state?</strong>
+                        <p>Props: Props are read only and immutable but it can be modified, <br />
+                        State: State changes can be asynchronous and mutable. It can be modified using this.setState.
+                        </p>
+                    </div>
+                    <div>
+                        <strong>3. What is the uses of UseEffect</strong>
+                        <p>The useEffect Hook always perform as SideEffect. For example: <br />
+                            Add an event listener for a button. <br />
+                            Data fetching from API when component mounts. <br />
+                            Perform an action when state or props change. <br />
+                            Clean up event listeners when the component unmounts.
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
