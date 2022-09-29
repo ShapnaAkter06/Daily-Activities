4d26b233e76a8562031559c2052c61289fd75a43
Shapna <shapna088@gmail.com>
2022-09-29 10:28:32 +0600

    added ques ans

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee, faBookOpenReader } from '@fortawesome/free-solid-svg-icons';
+import { faBookOpenReader } from '@fortawesome/free-solid-svg-icons';
 import './Home.css'
 import AllCards from '../All-Cards/AllCards';
 import MyInfo from '../MyInfo/MyInfo';
 import BreakTime from '../BreakTime/BreakTime';
 import ReadingDetails from '../ReadingDetails/ReadingDetails';
-import { library } from '@fortawesome/fontawesome-svg-core';
+
 
 const Home = () => {
     const [cards, setCards] = useState([]);
@@ -41,6 +41,24 @@ const Home = () => {
                     <div>
                         <ReadingDetails details={details}></ReadingDetails>
                     </div>
+                </div>
+            </div>
+            <div className="ques-ans">
+                <div>
+                    <strong>1. How does react work</strong>
+                    <p>ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p> <br />
+                    <strong>2. Difference between props and state?</strong>
+                    <p>Props: Props are read only, It can be modified, Props Immutable <br />
+                    State: State changes can be asynchronous. It can be modified using this.setState. State Mutuable.
+                    </p>
+                    <strong>3. What is the uses of UseEffect</strong>
+                    <p>The useEffect Hook always perform as SideEffect. For example: <br />
+                        Add an event listener for a button. <br />
+                        Data fetching from API when component mounts. <br />
+                        Perform an action when state or props change. <br />
+                        Clean up event listeners when the component unmounts.
+
+                    </p>
                 </div>
             </div>
         </div>
